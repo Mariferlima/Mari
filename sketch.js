f7e05d9f025869cd3c232b6457fdcb505b442387
Mariferlima <00001108799115sp@al.educacao.sp.gov.br>
2024-08-16 17:41:33 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+background("purple");
+}
+function draw() {
+
+
+
+
+stroke ("silver");
+fill("violet");
+//  console.log(mouseIsPressed)
+  if (mouseIsPressed) {
+rect(mouseX, mouseY, 25, 22)
+  }
+}
